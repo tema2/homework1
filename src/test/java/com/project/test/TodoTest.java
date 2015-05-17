e8685d10ac142e9824300813a5307f52ee58a008
tema2 <a.grabenko@gmail.com>
2015-05-17 21:11:05 +0300

    version 2

--- src/test/java/com/project/test/TodoTest.java
+++ src/test/java/com/project/test/TodoTest.java
@@ -1,44 +1,46 @@
 package com.project.test;
 
+import com.codeborne.selenide.ElementsCollection;
 import org.junit.Test;
-import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.CollectionCondition.empty;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TodoTest {
 
+    public void addTask(String task) {
+        $("#new-todo").setValue(task).pressEnter();
+    }
+
+    ElementsCollection taskList = $$("#todo-list>li");
+
     @Test
     public void testCreateTask() {
 
         open("http://todomvc.com/examples/troopjs_require/#/");
-        $("#new-todo").setValue("do1").pressEnter();
-        $("#new-todo").setValue("do2").pressEnter();
-        $("#new-todo").setValue("do3").pressEnter();
-        $("#new-todo").setValue("do4").pressEnter();
+        addTask("do1");
+        addTask("do2");
+        addTask("do3");
+        addTask("do4");
 
-        $$("#todo-list>li").shouldHaveSize(4);
-        $$("#todo-list>li").get(0).shouldHave(text("do1"));
-        $$("#todo-list>li").get(1).shouldHave(text("do2"));
-        $$("#todo-list>li").get(2).shouldHave(text("do3"));
-        $$("#todo-list>li").get(3).shouldHave(text("do4"));
+        taskList.shouldHave(texts("do1", "do2", "do3", "do4"));
 
-        $$("#todo-list>li").findBy(text("do2")).hover();
-        $$("#todo-list>li").findBy(text("do2")).find(".destroy").click();
+        taskList.findBy(text("do2")).hover();
+        taskList.findBy(text("do2")).find(".destroy").click();
 
-        $$("#todo-list>li").shouldHaveSize(3);
-        $$("#todo-list>li").get(0).shouldHave(text("do1"));
-        $$("#todo-list>li").get(1).shouldHave(text("do3"));
-        $$("#todo-list>li").get(2).shouldHave(text("do4"));
+        taskList.shouldHave(texts("do1", "do3", "do4"));
 
-        $$("#todo-list>li").find(text("do4")).find(".toggle").click();
-
+        taskList.find(text("do4")).find(".toggle").click();
         $("#clear-completed").click();
+        taskList.shouldHave(texts("do1", "do3"));
 
         $("#toggle-all").click();
         $("#clear-completed").click();
+
+        taskList.shouldBe(empty);
     }
 }
