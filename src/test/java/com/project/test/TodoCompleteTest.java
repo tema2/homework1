59a37ba3fe9307097bc5057c54f9a45e9f99a93b
tema2 <a.grabenko@gmail.com>
2015-05-18 19:54:05 +0300

    homework 2, version 2 methods added for: toggle delete
    changed filters methods

--- src/test/java/com/project/test/TodoCompleteTest.java
+++ src/test/java/com/project/test/TodoCompleteTest.java
@@ -16,17 +16,44 @@ public class TodoCompleteTest {
     public void addTask(String task) {
         $("#new-todo").setValue(task).pressEnter();
     }
-    public void gotoFilter(String filter) {
-        $(By.linkText(filter)).click();
+
+    public void filterAll(){
+       // $("[href='#/'").click();
+        $(By.linkText("All")).click();
+    }
+
+    public void filterActive(){
+        // $("[href='#/active'").click();
+        $(By.linkText("Active")).click();
+    }
+
+    public void filterCompleted(){
+        $(By.linkText("Completed")).click();
+        //  $("[href='#/completed'").click();
+    }
+
+    public void toggle(String task) {
+        taskList.find(text(task)).find(".toggle").click();
+    }
+
+    public void deleteTask(String task) {
+        taskList.findBy(text(task)).hover();
+        taskList.findBy(text(task)).find(".destroy").click();
     }
 
     ElementsCollection taskList = $$("#todo-list>li");
 
+    /* добавить проверку счетчика
+    public static void checkItemsLeftCounter(int number){
+        $("#todo-count>strong").shouldHave(exactText(Integer.toString(number)));
+    }
+     */
+
     @Test
     public void testCreateTask() {
-
-        //adding 5 tasks
         open("http://todomvc.com/examples/troopjs_require/#/");
+
+        //adding tasks
         addTask("do1");
         addTask("do2");
         addTask("do3");
@@ -34,107 +61,103 @@ public class TodoCompleteTest {
         addTask("do5");
         taskList.shouldHave(texts("do1", "do2", "do3", "do4", "do5"));
         //marking task 1 and 2 as completed and checking that total number of tasks is 5
-        taskList.find(text("do1")).find(".toggle").click();
-        taskList.find(text("do2")).find(".toggle").click();
+        toggle("do1");
+        toggle("do2");
         taskList.shouldHave(texts("do1", "do2", "do3", "do4", "do5"));
         //checking active tasks on Active filter
-        gotoFilter("Active");
+        filterActive();
         taskList.shouldHave(texts("", "", "do3", "do4", "do5"));
         //checking completed tasks on Completed filter
-        gotoFilter("Completed");
+        filterCompleted();
         taskList.shouldHave(texts("do1", "do2", "", "", ""));
 
         //marking third task as completed and checking filters
-        gotoFilter("All");
-        taskList.find(text("do3")).find(".toggle").click();
+        filterAll();
+        toggle("do3");
         taskList.shouldHave(texts("do1", "do2", "do3", "do4", "do5"));
-        gotoFilter("Active");
+        filterActive();
         taskList.shouldHave(texts("", "", "", "do4", "do5"));
-        gotoFilter("Completed");
+        filterCompleted();
         taskList.shouldHave(texts("do1", "do2", "do3", "", ""));
 
         //adding tasks on Active filter page
-        gotoFilter("Active");
+        filterActive();
         addTask("active1");
         addTask("active2");
         taskList.shouldHave(texts("", "", "", "do4", "do5", "active1", "active2"));
         //checking that two new tasks does not appeared on Completed filter and presented on All filter
-        gotoFilter("Completed");
+        filterCompleted();
         taskList.shouldHave(texts("do1", "do2", "do3", "", "", "", ""));
-        gotoFilter("All");
+        filterAll();
         taskList.shouldHave(texts("do1", "do2", "do3", "do4", "do5", "active1", "active2"));
 
         //adding tasks on Completed filter page, checking that they are not presented there
-        gotoFilter("Completed");
+        filterCompleted();
         addTask("completed1");
         addTask("completed2");
         taskList.shouldHave(texts("do1", "do2", "do3", "", "", "", "", "", ""));
-        gotoFilter("Active");
+        filterActive();
         taskList.shouldHave(texts("", "", "", "do4", "do5", "active1", "active2", "completed1", "completed2"));
-        gotoFilter("All");
+        filterAll();
         taskList.shouldHave(texts("do1", "do2", "do3", "do4", "do5", "active1", "active2", "completed1", "completed2"));
 
         //rechecking tasks, as a result do1-3 marked as active, active1-2, completed1-2 are marked as completed
-        taskList.find(text("do1")).find(".toggle").click();
-        taskList.find(text("do2")).find(".toggle").click();
-        taskList.find(text("do3")).find(".toggle").click();
-        taskList.find(text("active1")).find(".toggle").click();
-        taskList.find(text("active2")).find(".toggle").click();
-        taskList.find(text("completed1")).find(".toggle").click();
-        taskList.find(text("completed2")).find(".toggle").click();
+        toggle("do1");
+        toggle("do2");
+        toggle("do3");
+        toggle("active1");
+        toggle("active2");
+        toggle("completed1");
+        toggle("completed2");
         taskList.shouldHave(texts("do1", "do2", "do3", "do4", "do5", "active1", "active2", "completed1", "completed2"));
 
         //deleting completed tasks from Completed filter
-        gotoFilter("Completed");
+        filterCompleted();
         taskList.shouldHave(texts("", "", "", "", "", "active1", "active2", "completed1", "completed2"));
-        taskList.findBy(text("active1")).hover();
-        taskList.findBy(text("active1")).find(".destroy").click();
-        taskList.findBy(text("completed1")).hover();
-        taskList.findBy(text("completed1")).find(".destroy").click();
+        deleteTask("active1");
+        deleteTask("completed1");
         $("#clear-completed").click();
         taskList.shouldHave(texts("", "", "", "", ""));
 
         //complete from Active
-        gotoFilter("Active");
+        filterActive();
         taskList.shouldHave(texts("do1", "do2", "do3", "do4", "do5"));
-        taskList.find(text("do4")).find(".toggle").click();
-        taskList.find(text("do5")).find(".toggle").click();
+        toggle("do4");
+        toggle("do5");
         taskList.shouldHave(texts("do1", "do2", "do3", "", ""));
 
         //delete completed one by one from All
-        gotoFilter("All");
-        taskList.findBy(text("do4")).hover();
-        taskList.findBy(text("do4")).find(".destroy").click();
-        taskList.findBy(text("do5")).hover();
-        taskList.findBy(text("do5")).find(".destroy").click();
+        filterAll();
+        deleteTask("do4");
+        deleteTask("do5");
         taskList.shouldHave(texts("do1", "do2", "do3"));
 
         //delete completed from Active
-        gotoFilter("Active");
-        taskList.find(text("do3")).find(".toggle").click();
+        filterActive();
+        toggle("do3");
         $("#clear-completed").click();
-        gotoFilter("Completed");
+        filterCompleted();
         taskList.shouldHave(texts("", ""));
 
         //renew task from completed
-        gotoFilter("All");
-        taskList.find(text("do2")).find(".toggle").click();
-        gotoFilter("Completed");
+        filterAll();
+        toggle("do2");
+        filterCompleted();
         taskList.shouldHave(texts("", "do2"));
-        taskList.find(text("do2")).find(".toggle").click();
+        toggle("do2");
         taskList.shouldHave(texts("", ""));
-        gotoFilter("All");
+        filterAll();
         taskList.shouldHave(texts("do1", "do2"));
-        gotoFilter("Active");
+        filterActive();
         taskList.shouldHave(texts("do1", "do2"));
 
         //mark all as completed
-        gotoFilter("All");
+        filterAll();
         $("#toggle-all").click();
         taskList.shouldHave(texts("do1", "do2"));
-        gotoFilter("Active");
+        filterActive();
         taskList.shouldHave(texts("", ""));
-        gotoFilter("Completed");
+        filterCompleted();
         taskList.shouldHave(texts("do1", "do2"));
         $("#clear-completed").click();
         taskList.shouldBe(empty);
@@ -148,18 +171,18 @@ public class TodoCompleteTest {
         */
         addTask("task1");
         addTask("task2");
-        gotoFilter("All");
-        taskList.find(text("task2")).find(".toggle").click();
+        filterAll();
+        toggle("task2");
         taskList.shouldHave(texts("task1", "task2"));
-        gotoFilter("Active");
+        filterActive();
         taskList.shouldHave(texts("task1", ""));
         $("#toggle-all").click();
         taskList.shouldHave(texts("", ""));
-        gotoFilter("Completed");
+        filterCompleted();
         taskList.shouldHave(texts("task1", "task2"));
         $("#toggle-all").click();
         taskList.shouldHave(texts("", ""));
-        gotoFilter("Active");
+        filterActive();
         taskList.shouldHave(texts("task1", "task2"));
 
         //edit task...
